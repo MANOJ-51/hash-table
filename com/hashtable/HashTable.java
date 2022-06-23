f3933c03f5f4586c7e9af9c519bfb900dfb042c6
MANOJ-51 <m4anoj51@gmail.com>
2022-06-23 23:53:16 +0530

    added: method to find frequency of each word in a paragraph

--- com/hashtable/HashTable.java
+++ com/hashtable/HashTable.java
@@ -6,10 +6,10 @@ import java.util.Map;
 public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome to Hash Table");
+        System.out.println("");
+        System.out.println("Frequency of words /Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations/ is  :");
 
-        System.out.println("Frequency of words /to be or not to be/ is  :");
-
-        String string = "To be or not to be";
+        String string = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         Map<String, Integer> hashTable = new Hashtable<>();               //initialising hashtable
         String[] words = string.toLowerCase().split(" ");  //changing words to lower case and split
         for (String word : words) {                                  // repeat for every single word
